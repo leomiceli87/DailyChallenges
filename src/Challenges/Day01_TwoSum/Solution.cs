cdb341a9eff4bc953f66a423b939892f8954a0d1
LEONARDO V MICELI <42792113+leomiceli87@users.noreply.github.com>
2026-07-02 21:50:03 -0400

    First Part

---
+++ src/Challenges/Day01_TwoSum/Solution.cs
@@ -0,0 +1,11 @@
+namespace Challenges.Day01_TwoSum;
+
+public static class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        // sua implementação aqui
+
+        return [0, 0];
+    }
+}
